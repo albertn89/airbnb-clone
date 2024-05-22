c99a4d7c969a19f3bd7617c9c8ba8b9e29218c3c
Albert Negoro <albert.negoro89@gmail.com>
2024-05-22 23:34:14 +0700

    social sign in

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import axios from "axios";
-import { useCallback, useState } from "react";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 
@@ -9,10 +10,10 @@ import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 
 import useRegisterModal from "@/app/hooks/useRegisterModal";
-import Modal from "./Modal";
+import Button from "../Button";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
-import Button from "../Button";
+import Modal from "./Modal";
 
 const RegisterModal = () => {
 	const registerModal = useRegisterModal();
@@ -80,8 +81,18 @@ const RegisterModal = () => {
 	const footerContent = (
 		<div className="flex flex-col gap-4 mt-3">
 			<hr />
-			<Button outline label="Continue with Google" icon={FcGoogle} onClick={() => {}} />
-			<Button outline label="Continue with Github" icon={AiFillGithub} onClick={() => {}} />
+			<Button
+				outline
+				label="Continue with Google"
+				icon={FcGoogle}
+				onClick={() => signIn("google")}
+			/>
+			<Button
+				outline
+				label="Continue with Github"
+				icon={AiFillGithub}
+				onClick={() => signIn("github")}
+			/>
 
 			<div className="text-neutral-500 text-center mt-4 font-light">
 				<div className="justify-center flex flex-row items-center gap-2">
